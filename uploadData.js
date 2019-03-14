6e47fffc4d6f3cc892268d2a02ea4f9294629fd9
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-14 09:56:17 -0400

    Change id's of surname and module in order to fix upload data alert

--- uploadData.js
+++ uploadData.js
@@ -3,5 +3,5 @@ function startDataUpload(){
 	var surname=document.getElementById("surname").value;
 	var module=document.getElementById("module").value;
 	var postString="name="+name+"&surname="+surname+"&module="+module;
-	alert("postString");
+	alert(postString);
 }
