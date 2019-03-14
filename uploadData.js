f7b46b50a97030fe05d9a05b57d4a6b243ec1072
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-14 10:18:20 -0400

    Call the function to process the form answers

--- uploadData.js
+++ uploadData.js
@@ -4,6 +4,7 @@ function startDataUpload(){
 	var module=document.getElementById("module").value;
 	var postString="name="+name+"&surname="+surname+"&module="+module;
 	alert(postString);
+	processData(postString);
 }
 
 var client; //the global variable that holds the request
